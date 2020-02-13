2c76b1341bb3aaf75e43320895332212b9bc3b43
Jack <jackrrye@gmail.com>
2020-02-13 13:13:30 -0600

    started work on sightings new form

--- app/models/park.rb
+++ app/models/park.rb
@@ -2,4 +2,9 @@ class Park < ApplicationRecord
     has_many :sightings
     has_many :users, through: :sightings
     has_many :species, through: :sightings
+
+    validates :name, presence: true
+    validates :size, presence: true
+    validates :location, presence: true
+
 end
--- app/models/species.rb
+++ app/models/species.rb
@@ -2,4 +2,7 @@ class Species < ApplicationRecord
     has_many :sightings
     has_many :users, through: :sightings
     has_many :parks, through: :sightings
+
+    validates :name, presence: true
+    validates :kingdom, presence: true
 end
