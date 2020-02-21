a807c8d44ecf187eff13cd5c27aaab1a36216bbe
Jack <jackrrye@gmail.com>
2020-02-20 20:47:19 -0600

    added a readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Sightings#create
+# ParkTrack
+
+This app is designed for a person who visits national parks to be able to track the species they see there. 
+
+
+## Architecture and models
+
+The main 3 models in this project are Sightings, Parks, and Species. The Sightings table functions as a join table between Parks, Species, and Users. 
+
+
+## Development
+
+Devise and Omniauth were used to create this project. 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'sightings/show'
-  get 'sightings/new'
-  get 'sightings/edit'
-  get 'users/show'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }  
   
-  get 'species/index'
-  get 'species/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
     resources :parks, only: [:index, :show] do
@@ -20,33 +14,8 @@ Rails.application.routes.draw do
 
     get '/dashboard', to: 'users#show' 
 
-    # resources :users, only: [:show]
-
-    # resources :sessions, only: [:new, :create, :destroy]
-
     root 'application#hello'
 
-    #show/update/destroy
-    # sighting/1
-
-    #edit
-    # sighting/1/edit
-
-    #new -- this is where new parks or new species would be created
-    # parks/1/sightings/new
-    # sightings/new
-
-    #index
-    # sightings/
-    # parks/1/sightings
-
-    # species/
-
-    # parks/
-
-    #git add .
-    #git commit -m "message"
-    #git push
 
 
 
